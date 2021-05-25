3aaf7b9325d6134e320c249fdb08dfc81220d0cd
OG-Gardavsky <ogardavsky@gmail.com>
2021-05-25 23:00:09 +0200

    as function

--- shopplist/addlist.php
+++ shopplist/addlist.php
@@ -14,7 +14,24 @@ require 'inc/db.php';
 
 
     function displayRows($rowCount) {
+        for ($i = 0; $i<$rowCount; $i++) {
+            echo '<div class="flexRow" >';
+            $itemNum = $i + 1;
 
+            echo ' <div class="form-group">';
+            echo '<label for="neco">'.$itemNum.'. item</label>';
+            echo '<input type="text" name="'.$itemNum.'itemName" id="neco" required class="form-control" value="'
+                .htmlspecialchars(@$_POST[$itemNum.'itemName']).'"/>';
+            echo '</div>';
+
+            echo '<div class="form-group">';
+            echo '<label for="name">count</label>';
+            echo '<input type="number" name="'.$itemNum.'itemCount" id="name" required class="form-control" value="'
+                .htmlspecialchars(@$_POST[$itemNum.'itemCount']).'"/>';
+            echo '</div>';
+
+            echo '</div>';
+        }
     }
 
 
@@ -27,7 +44,7 @@ require 'inc/db.php';
         <div class="form-group">
             <label for="name">Name of list</label>
             <input type="text" name="nameOfList" id="name" required class="form-control"
-                value=" <?php echo htmlspecialchars(@$_POST['nameOfList']) ?> "
+                value="<?php echo htmlspecialchars(@$_POST['nameOfList']) ?>"
             />
         </div>
 
@@ -52,32 +69,42 @@ require 'inc/db.php';
 
 
         <?php
-            $numberOfRows = 1;
-
-            //udelat funkci ktera bude pri zvetseni cisla itereaci o i prekreslova celej seznam
 
 
-
-            for ($i = 0; $i<$numberOfRows; $i++) {
-                echo '<div class="flexRow" >';
-                $itemNum = $i + 1;
-
-                echo ' <div class="form-group">';
-                echo '<label for="neco">'.$itemNum.'. item</label>';
-                echo '<input type="text" name="'.$itemNum.'itemName" id="neco" required class="form-control" value="'
-                    .htmlspecialchars(@$_POST[$itemNum.'itemName']).'"/>';
-                echo '</div>';
-
-                echo '<div class="form-group">';
-                echo '<label for="name">count</label>';
-                echo '<input type="number" name="'.$itemNum.'itemCount" id="name" required class="form-control" value="'
-                    .htmlspecialchars(@$_POST[$itemNum.'itemCount']).'"/>';
-                echo '</div>';
-
-                echo '</div>';
+            if (empty(@$_POST['numberOfRows'])) {
+                @$_POST['numberOfRows'] = 1;
             }
 
+            echo @$_POST['numberOfRows'];
+
+            displayRows(@$_POST['numberOfRows']);
+
         ?>
+
+<!--        <button id="addRowBtn" class="btn btn-secondary" onclick=" --><?php // @$_POST['numberOfRows'] ++ ?><!-- ">add item</button>-->
+<!--        <button id="addRowBtn" class="btn btn-secondary" onclick=" --><?php // echo '<h1>blby toto</h1>' ?><!-- ">add item</button>-->
+
+
+        <?php
+
+        if(isset($_POST['button1'])) {
+            echo "This is Button1 that is selected";
+        }
+        if(isset($_POST['button2'])) {
+            echo "This is Button2 that is selected";
+        }
+        ?>
+
+        <form method="post">
+            <input type="submit" name="button1"
+                   value="Button1"/>
+
+            <input type="submit" name="button2"
+                   value="Button2"/>
+        </form>
+
+
+        <hr />
 
 
 
@@ -86,6 +113,9 @@ require 'inc/db.php';
         <a href="index.php" class="btn btn-light">cancel</a>
     </form>
 
+<!--<script>-->
+<!--    // document.querySelector('#addRowBtn').addEventListener('click', e => e.preventDefault());-->
+<!--</script>-->
 
 <?php
 include 'inc/footer.php';
