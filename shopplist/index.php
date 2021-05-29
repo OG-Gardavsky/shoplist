b33ddfc9dfa3ced2e0b3bd9fb374713b1d78a139
OG-Gardavsky <ogardavsky@gmail.com>
2021-05-29 13:26:44 +0200

    poc dispaly of shoplist

--- shopplist/index.php
+++ shopplist/index.php
@@ -5,6 +5,15 @@
 
 
 
+    $shopListsQuery = $db->prepare("SELECT * FROM sl_shop_lists WHERE user_id = ?");
+    try {
+        $shopListsQuery->execute([$currentUserId]);
+        $shopLists = $shopListsQuery->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch (Exception $exception) {
+        $errors['genericError'] = 'Unexpected application error';
+    }
+
 
     $pageTitle='';
     include 'inc/header.php';
@@ -12,11 +21,38 @@
 
 ?>
 
-    <h1>
-        your shopping lists
-    </h1>
+<!--    <h1>your shopping lists</h1>-->
 
     <a href="addlist.php" class="btn btn-primary">Add new shopping list</a>
+
+    <?php
+        foreach ($shopLists as $shopList) {
+//            echo '';
+//                echo '';
+//                echo '';
+//            echo '';
+
+        }
+    ?>
+
+    <div class="card">
+        <div class="card-header">
+            <div><span class="badge badge-info">zradlo</span></div>
+            some shooping list
+        </div>
+        <div class="card-body flexRow cardContent">
+<!--            <div><span class="badge badge-secondary">'.htmlspecialchars($post['category_name']).'</span></div>-->
+            <div><span class="badge badge-secondary">zradlo</span></div>
+            <span>category: jidlo</span>
+
+            <div>
+                <a href="#" class="btn btn-secondary">edit</a>
+                <a type="button" class="btn btn-success">✓</a>
+            </div>
+        </div>
+    </div>
+
+
 
 
 <?php
