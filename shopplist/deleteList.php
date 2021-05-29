28ec1a928eae27f2d75d3666a653af41fd7c7216
OG-Gardavsky <ogardavsky@gmail.com>
2021-05-29 15:18:20 +0200

    delete error handling

--- shopplist/deleteList.php
+++ shopplist/deleteList.php
@@ -2,16 +2,26 @@
     require 'inc/db.php';
     require 'user_required.php';
 
+    $errors = [];
+
     $shopListId = !empty($_REQUEST['shopListId'])?$_REQUEST['shopListId']:'';
 
     $shopListToDeleteQuery = $db->prepare("SELECT * FROM sl_shop_lists WHERE id = ? AND user_id = ? LIMIT 1");
     try {
         $shopListToDeleteQuery->execute([$shopListId, $currentUserId]);
-        $shopListToDelete = $shopListToDeleteQuery->fetch(PDO::FETCH_ASSOC);
+
+        if ($shopListToDeleteQuery->rowCount() == 0) {
+            $errors['genericError'] = 'Shopping list does not exist';
+        } else {
+            $shopListToDelete = $shopListToDeleteQuery->fetch(PDO::FETCH_ASSOC);
+        }
 
     } catch (Exception $exception) {
+        echo $exception;
         $errors['genericError'] = 'Unexpected application error';
     }
+
+
 
 
 
@@ -19,13 +29,19 @@
     include 'inc/header.php';
 
 
+    if (isset($shopListToDelete)) {
+        echo '<div class="alert alert-info">Are you sure you want to delete:"'.
+              $shopListToDelete['name']
+            .'" shopping list(including all items)</div>';
+    }
+
 
 
 ?>
 
-    <div class="alert alert-info">
-        Are you sure you want to delete: "<?php echo $shopListToDelete['name'] ?>" shopping list(including all items)?
-    </div>
+<!--    <div class="alert alert-info">-->
+<!--        Are you sure you want to delete: "--><?php //if (isset($shopListToDelete)) { $shopListToDelete['name']; }?><!--" shopping list(including all items)?-->
+<!--    </div>-->
 
 <?php if (!empty($errors['genericError'])) { echo '<div class="alert alert-danger">'.$errors['genericError'].'</div>';  } ?>
 
