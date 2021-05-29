6b654a3595367e95bb791003379915fded2375a9
OG-Gardavsky <ogardavsky@gmail.com>
2021-05-29 15:50:38 +0200

    delete modification

--- shopplist/deleteList.php
+++ shopplist/deleteList.php
@@ -2,44 +2,53 @@
     require 'inc/db.php';
     require 'user_required.php';
 
-    $errors = [];
-
-    $shopListId = !empty($_REQUEST['shopListId'])?$_REQUEST['shopListId']:'';
-
-    $shopListToDeleteQuery = $db->prepare("SELECT * FROM sl_shop_lists WHERE id = ? AND user_id = ? LIMIT 1");
-    try {
-        $shopListToDeleteQuery->execute([$shopListId, $currentUserId]);
-
-        if ($shopListToDeleteQuery->rowCount() == 0) {
-            $errors['genericError'] = 'Shopping list does not exist';
-        } else {
-            $shopListToDelete = $shopListToDeleteQuery->fetch(PDO::FETCH_ASSOC);
-        }
-
-    } catch (Exception $exception) {
-        echo $exception;
-        $errors['genericError'] = 'Unexpected application error';
-    }
-
-
-
-
-
     $pageTitle='Delete shop list';
     include 'inc/header.php';
 
+    $errors = [];
 
-    if (isset($shopListToDelete)) {
-        echo '<div class="alert alert-info">Are you sure you want to delete:"'.
-              $shopListToDelete['name']
-            .'" shopping list(including all items)</div>';
+    $shopListId = !empty($_REQUEST['shopListId'])?$_REQUEST['shopListId']:null;
+
+
+    if ($shopListId != null) {
+
+        $shopListToDeleteQuery = $db->prepare("SELECT * FROM sl_shop_lists WHERE id = ? AND user_id = ? LIMIT 1");
+        try {
+            $shopListToDeleteQuery->execute([$shopListId, $currentUserId]);
+
+            if ($shopListToDeleteQuery->rowCount() == 0) {
+                $errors['genericError'] = 'Shopping list does not exist';
+            } else {
+                $shopListToDelete = $shopListToDeleteQuery->fetch(PDO::FETCH_ASSOC);
+            }
+
+        } catch (Exception $exception) {
+            echo $exception;
+            $errors['genericError'] = 'Unexpected application error';
+        }
+
+        if (isset($shopListToDelete)) {
+            echo '<div class="alert alert-info">Are you sure you want to delete:"'.
+                $shopListToDelete['name']
+                .'" shopping list(including all items)</div>';
+        }
+
+    } else {
+        $errors['genericError'] = 'No shop list to delete';
     }
+
+
+
+
 
     if (!empty($errors['genericError'])) {
         echo '<div class="alert alert-danger">' . $errors['genericError'] . '</div>';
     }
 
+
 ?>
+
+    <input type="submit" class="btn btn-secondary" name="addRowBtn" value="Add item" />
 
 
 
